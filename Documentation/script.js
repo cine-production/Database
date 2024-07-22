a94934ddf75ec1121f1c89862cea6882ecbe10cc
cineprod <soundtrackdlp.contact@gmail.com>
2024-07-22 13:11:56 +0200

    NEW UPDATE

--- Documentation/script.js
+++ Documentation/script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const overlay = document.getElementById('overlay');
-    const passwordModal = document.getElementById('passwordModal');
+    const passwordModalcust = document.getElementById('passwordModalcust');
     const checkPasswordButton = document.getElementById('checkPassword');
     const passwordInput = document.getElementById('passwordInput');
+    const main = document.getElementById('mainContainer');
 
+    main.style.zIndex = '0';
     // Fonction pour vérifier si le mot de passe est toujours valide
     function isPasswordValid() {
         const timestamp = localStorage.getItem('passwordTimestamp');
@@ -25,8 +27,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Si le mot de passe n'a pas été entré ou a expiré, affiche le popup et l'overlay
     if (!isPasswordEntered) {
-        passwordModal.style.display = 'block';
+        passwordModalcust.style.display = 'block';
         overlay.style.display = 'block';
+        main.style.zIndex = '0';
     }
 
     checkPasswordButton.addEventListener('click', () => {
@@ -36,11 +39,38 @@ document.addEventListener('DOMContentLoaded', function() {
             // Enregistre que le mot de passe a été entré avec succès et le timestamp actuel
             localStorage.setItem('isPasswordEntered', 'true');
             localStorage.setItem('passwordTimestamp', new Date().getTime().toString());
-            passwordModal.style.display = 'none';
+            passwordModalcust.style.display = 'none';
             overlay.style.display = 'none';
+            main.style.zIndex = '1';
         } else {
             alert('Mot de passe incorrect. Veuillez réessayer.');
             passwordInput.value = '';
         }
     });
 });
+
+// Fonction pour faire défiler la page jusqu'à une section
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  // Fonction pour faire défiler la page vers le haut
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
+  // Gestion de l'affichage du bouton de retour en haut avec un effet de fondu
+  window.onscroll = function () {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = 'block';
+      scrollToTopBtn.classList.add('show');
+    } else {
+      scrollToTopBtn.classList.remove('show');
+    }
+  };
+
+  function navBarFunction() {
+    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+}
